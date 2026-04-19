7d52b690ecc5754e0848f3f50b5ea0332d3415fb
Mateusz <75093547+MeG123321@users.noreply.github.com>
2026-04-19 21:42:32 +0200

    Poprawa

--- Magazyn/obj/Debug/net10.0/Magazyn.AssemblyInfo.cs
+++ Magazyn/obj/Debug/net10.0/Magazyn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Magazyn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7aa715ce9cc1ece526c5b99f85eaf142ea9d3c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fb8f6399542222ac024410478f69030da9c9165")]
 [assembly: System.Reflection.AssemblyProductAttribute("Magazyn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Magazyn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
