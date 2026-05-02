0776e719d62d02e4486ac2ffc5bbbe3e4b8246a0
Mateusz <75093547+MeG123321@users.noreply.github.com>
2026-05-02 13:44:18 +0200

    moduł 4 podstawy

--- Magazyn/Program.cs
+++ Magazyn/Program.cs
@@ -16,24 +16,27 @@ builder.Services.AddAuthentication(Microsoft.AspNetCore.Authentication.Cookies.C
             : Microsoft.AspNetCore.Http.CookieSecurePolicy.Always;
     });
 
+// HttpClient (MUSI być przed builder.Build())
+builder.Services.AddHttpClient("Mailtrap", client =>
+{
+    client.BaseAddress = new Uri("https://send.api.mailtrap.io/");
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthentication();
-
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -43,11 +46,4 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-
 app.Run();
-
-
-builder.Services.AddHttpClient("Mailtrap", client =>
-{
-    client.BaseAddress = new Uri("https://send.api.mailtrap.io/");
-});
